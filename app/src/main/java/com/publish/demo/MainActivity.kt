de0075d45756308daf1d73b436b0163c813ce1f2
Felix <chuanfa.chai@gmail.com>
2022-05-31 18:00:04 +0800

    test

--- app/src/main/java/com/publish/demo/MainActivity.kt
+++ app/src/main/java/com/publish/demo/MainActivity.kt
@@ -14,6 +14,7 @@ import com.publish.demo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
